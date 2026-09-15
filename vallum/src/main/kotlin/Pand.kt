696bd74b611cd0f8f24c1316c0c33be65a29a6d9
Erik van Velzen <erik@evanv.nl>
2026-09-15 14:30:25 +0200

    fix(vallum): remove bag+jackson dependency
    Vallum had an transitive dependency on Jackson via the library
    com.zenmo.bag. This lead to a version conflict in LUX.
    
    This dependency was included in this way because AnyLogic lacked
    thorough dependency management. Now that we no longer use AnyLogic,
    it can be removed here.
    
    The function `allPandIdsOfSurveys` has been made public.

--- vallum/src/main/kotlin/Pand.kt
+++ vallum/src/main/kotlin/Pand.kt
@@ -1,16 +1,13 @@
 package com.zenmo.vallum
 
 import com.zenmo.zummon.companysurvey.Survey
-import com.zenmo.bag.Pand
-import com.zenmo.bag.byPandIds
 
-private fun allPandIdStrings(surveys: List<Survey>): Set<String> = surveys
+/**
+ * Get all BAG pand ID's in a list of surveys.
+ * This result can be used to efficiently query the BAG for the geometry and metadata.
+ * For example, using the library https://github.com/Zenmo/bag-client-java
+ */
+fun allPandIdsOfSurveys(surveys: List<Survey>): Set<String> = surveys
     .flatMap { it.allPandIds() }
     .map { it.value }
     .toSet()
-
-fun fetchBagPanden(surveys: List<Survey>): Map<String, Pand> {
-    val pandIs = allPandIdStrings(surveys)
-
-    return byPandIds(pandIs)
-}
